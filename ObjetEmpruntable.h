3f7ab0de55fb5941b5aa2c3aebca35862d3af4ca
stephanieleclerc <stephanie.leclerc@polymtl.ca>
2016-03-29 01:22:48 -0400

    add commentaires ObjetEmpruntable

--- ObjetEmpruntable.h
+++ ObjetEmpruntable.h
@@ -1,3 +1,10 @@
+/****************************************************************************
+* Fichier: ObjetEmpruntable.h
+* Auteur(s): Charles-Olivier Favreau et Stéphanie Leclerc
+* Date de creation: 10 mars 2016
+* Date de modification: 28 mars 2016
+* Description: Description de la classe ObjetEmpruntable
+****************************************************************************/
 #ifndef OBJETEMPRUNTABLE_H
 #define OBJETEMPRUNTABLE_H
 
