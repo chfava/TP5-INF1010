bc48032851c592b395efea0956242c82349d2d48
stephanieleclerc <stephanie.leclerc@polymtl.ca>
2016-03-29 01:24:45 -0400

    add commentaires ObjetNumerique

--- ObjetNumerique.h
+++ ObjetNumerique.h
@@ -1,3 +1,10 @@
+/****************************************************************************
+* Fichier: ObjetNumerique.h
+* Auteur(s): Charles-Olivier Favreau et Stéphanie Leclerc
+* Date de creation: 10 mars 2016
+* Date de modification: 28 mars 2016
+* Description: Description de la classe ObjetNumerique
+****************************************************************************/
 #ifndef	OBJETNUMERIQUE_H
 #define OBJETNUMERIQUE_H
 
