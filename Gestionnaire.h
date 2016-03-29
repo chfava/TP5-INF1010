5a32bd26f2f93be4754a92bf1284b6e3add5b27f
stephanieleclerc <stephanie.leclerc@polymtl.ca>
2016-03-29 03:05:27 -0400

    add commentaires Gest

--- Gestionnaire.h
+++ Gestionnaire.h
@@ -18,12 +18,26 @@
 template <typename T>
 class Gestionnaire {
 public:
-
+	/****************************************************************************
+	* Fonction: Gestionnaire::Gestionnaire
+	* Description: Constructeur par défaut
+	* Paramètres: aucun
+	* Retour: aucun
+	****************************************************************************/
 	Gestionnaire() {};
-
+	/****************************************************************************
+	* Fonction: Gestionnaire::Gestionnaire
+	* Description: Destructeurs
+	* Paramètres: aucun
+	* Retour: aucun
+	****************************************************************************/
 	~Gestionnaire() {};
-
-
+	/****************************************************************************
+	* Fonction: Gestionnaire::ajouterElement
+	* Description: Ajoute un pointeur seulement s'il n'est pas dans le conteneur
+	* Paramètres: - T objet: un pointeur d'objet
+	* Retour: (bool) true si l'ajout est fait, false sinon
+	****************************************************************************/
 	bool ajouterElement(T* objet) {
 		std::list<T*>::iterator pos;
 		for (pos = listeObjets.begin(); pos != listeObjets.end(); ++pos) {
@@ -34,7 +48,12 @@ public:
 		listeObjets.push_back(objet);
 		return true;
 	};
-
+	/****************************************************************************
+	* Fonction: Gestionnaire::retirerElement
+	* Description: Retire un pointeur d'un conteneur
+	* Paramètres: - T objet: un pointeur d'objet
+	* Retour: (bool) true si l'objet est retiré, false sinon
+	****************************************************************************/
 	bool retirerElement(T* objet) {
 		std::list<T*>::iterator pos;
 		for (pos = listeObjets.begin(); pos != listeObjets.end(); ++pos) {
@@ -45,7 +64,12 @@ public:
 		}
 		return false;
 	};
-
+	/****************************************************************************
+	* Fonction: Gestionnaire::retirerContenu
+	* Description: Retire l'objet ou les objets qui vérifient la condition
+	* Paramètres: - predicate condition: une condition à vérifier
+	* Retour: (bool) true si les objets sont retirés, false sinon
+	****************************************************************************/
 	template <typename predicate>
 	bool retirerContenu(predicate& condition) {
 	bool elementRetiré;
@@ -57,41 +81,56 @@ public:
 		}
 	}
 	return elementRetiré;
-};
-
-template <typename predicate>
-T* trouverElement(predicate& condition)const {
-	std::list<T*>::iterator pos;
-	for (pos = listeObjets.begin(); pos != listeObjets.end(); ++pos) {
-		if (condition(*pos)){
-			return *pos;
+	};
+	/************************************************************************************
+	* Fonction: Gestionnaire::trouverElement
+	* Description: Recherche l'objet qui vérifie le prédicat et retourne son pointeur
+	* Paramètres: - predicate condition: une condition à vérifier
+	* Retour: (T*) le pointeur de l'objet qui vérifie la condition
+	*************************************************************************************/
+	template <typename predicate>
+	T* trouverElement(predicate& condition)const {
+		std::list<T*>::iterator pos;
+		for (pos = listeObjets.begin(); pos != listeObjets.end(); ++pos) {
+			if (condition(*pos)){
+				return *pos;
+			}
 		}
-	}
-	return nullptr;
+		return nullptr;
 	
-}
-
-bool trouverElement(T objet)const {
-	std::list<T*>::iterator pos;
-	for (pos = listeObjets.begin(); pos != listeObjets.end(); ++pos) {
-		if (*pos == objet){
-			return true;
-		}
 	}
-	return false;
-}
-
-template <typename predicate>
-std::list<T*> trouverContenu(predicate condition)const{
-	std::list <T*> liste;
-	std::list<T*>::iterator pos;
-	for (pos = listeObjets.begin(); pos != listeObjets.end(); ++pos) {
-		if (condition){
-			liste.push_back(*pos);
+	/****************************************************************************
+	* Fonction: Gestionnaire::trouverElement
+	* Description: Recherche l'objet
+	* Paramètres: - T objet: un objet à trouver
+	* Retour: (bool) true si l'objet a été trouvé, false sinon
+	****************************************************************************/
+	bool trouverElement(T objet)const {
+		std::list<T*>::iterator pos;
+		for (pos = listeObjets.begin(); pos != listeObjets.end(); ++pos) {
+			if (*pos == objet){
+				return true;
+			}
 		}
+		return false;
 	}
-	return liste;
-}
+	/***********************************************************************************************************
+	* Fonction: Gestionnaire::trouverContenu
+	* Description: Recherche tous les objets qui vérifient une condition et retourne une liste de pointeurs
+	* Paramètres: - predicate condition
+	* Retour: (list <T*>) une liste de pointeurs des objets qui vérifient une condition
+	***********************************************************************************************************/
+	template <typename predicate>
+	std::list<T*> trouverContenu(predicate condition)const{
+		std::list <T*> liste;
+		std::list<T*>::iterator pos;
+		for (pos = listeObjets.begin(); pos != listeObjets.end(); ++pos) {
+			if (condition){
+				liste.push_back(*pos);
+			}
+		}
+		return liste;
+	}
 
 private: 
 	std::list <T*> listeObjets;
