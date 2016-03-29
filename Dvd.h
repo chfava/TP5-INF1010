ee339ced686784778f3c37cdfff5739f1fb3142c
stephanieleclerc <stephanie.leclerc@polymtl.ca>
2016-03-29 00:28:42 -0400

    add entete Dvd

--- Dvd.h
+++ Dvd.h
@@ -1,3 +1,10 @@
+/****************************************************************************
+* Fichier: Dvd.h
+* Auteur(s): Charles-Olivier Favreau et Stéphanie Leclerc
+* Date de creation: 10 mars 2016
+* Date de modification: 28 mars 2016
+* Description: Description de la classe Dvd
+****************************************************************************/
 #ifndef DVD_H
 #define DVD_H
 #include "ObjetEmpruntable.h"
